60b1f2a32545c6352a8d55176eafd2196c5adb28
John Lee <jleeowl@gmail.com>
2022-04-27 12:32:46 +0800

    Refactor ping.mjs to esm

--- src/server/api/ping.mjs
+++ src/server/api/ping.mjs
@@ -1,12 +1,44 @@
 import express from 'express'
 import db from '../../database.mjs'
 
+/**
+ * Express Router to mount ping related functions
+ * @constant
+ * @namespace pingRouter
+ * @type {express.Router}
+ */
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({
-    data: 'pong'
-  })
-})
+/**
+ * Express router class providing ping related routes
+ * @requires express
+ */
+class Ping {
+  /**
+   * Create the Ping
+   * @constructor
+   */
+  constructor() {
+    // Routes are defined in the constructor
 
-export default router
+    /**
+     * @name get/ping
+     * @function
+     * @inner
+     * @param {string} path - Express path
+     * @param {callback} middleware - Express middleware.
+     */
+    router.get('/', async (req, res, next) => {
+      res.json({
+        data: 'pong'
+      })
+    })
+
+    return router
+  }
+}
+
+/**
+ * Returns the Ping router
+ */
+export default new Ping()
