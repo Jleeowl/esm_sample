b576792b14d41c1c37cbafa550bdfadbeb63caba
John Lee <jleeowl@gmail.com>
2022-05-12 14:35:34 +0800

    Added package for uuid and updated migration to auto generate uuid on entry creation

--- migrations/20220511055518_create_tenants_table.mjs
+++ migrations/20220511055518_create_tenants_table.mjs
@@ -1,3 +1,5 @@
+import uuid from 'uuid'
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -6,11 +8,12 @@ export function up(knex) {
   return knex.schema.createTable('tenants', t => {
     t.increments()
     
-    t.string('slug').defaultTo('').index()
+    t.string('uuid').notNullable().defaultTo(uuid.v4()).unique().index()
+    t.string('slug').defaultTo('').unique().index()
     t.string('db_type').notNullable().defaultTo('pg')
-    t.string('db_name').notNullable().defaultTo('')
-    t.string('db_host').notNullable().defaultTo('')
-    t.string('db_user').notNullable().defaultTo('')
+    t.string('db_name').notNullable().defaultTo('testdb')
+    t.string('db_host').notNullable().defaultTo('127.0.0.1')
+    t.string('db_user').notNullable().defaultTo('postgres')
     t.string('db_password').notNullable().defaultTo('')
     t.integer('db_port').notNullable().defaultTo(5432)
 
